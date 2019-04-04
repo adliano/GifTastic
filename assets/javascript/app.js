8c601a5962a41516c4a94620ae53d2eb9dedb715
Adliano Alves <adliano@me.com>
2019-04-03 20:42:13 -0700

    clear some comments and add some comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,29 +1,17 @@
 // Data Set with initial animals, Set used to void repetitive input
 let animalsSet = new Set(["cow", "dog", "cat", "rat", "horse", "monkey", "bat"]);
-// API Key
-const API_KEY = "GvtvtZwH7qHmYmA0qVRIs8mOOul36if";
-// Total of images to display
-const IMG_LIMIT = 1;
-
-/* ************************************************************* */
-/* * * * * * * * * * * * reloadButtons() * * * * * * * * * * * * */
-/* ************************************************************* */
-// This function will read each element from animalsSet and create a button
-// for each animal
-function reloadButtons() {
-  // Clean Screen to remove current buttons
-  document.querySelector("#btnContainer").innerHTML = "";
-  // Loop trough Set and add new Buttons
-  for (let animal of animalsSet) {
-    let _btn = mkbtn(animal);
-    document.querySelector("#btnContainer").appendChild(_btn);
-  }
-}
+// JSOn to hold URL parameters
+let queryURL = {
+  url: "http://api.giphy.com/v1/gifs/search?",
+  apiKey: "GvtvtZwH7qHmYmA0qVRIs8mOOul36if",
+  limit: 10,
+};
 /* ************************************************************* */
 /* * * * * * * * * * * * * * mkbtn() * * * * * * * * * * * * * * */
 /* ************************************************************* */
 // function to create a new button, the argument passed to this function will                                 
 // be the text displayed on button
+// <button class="btn btn-primary m-2">textContent</button>
 function mkbtn(textContent) {
   // create new element button
   let _btn = document.createElement("button");
@@ -39,6 +27,11 @@ function mkbtn(textContent) {
 /* ************************************************************* */
 // function to create a new img element, the passed argument to this function                                      
 // will be the JSON data object from API and innerHTML from the button 
+// <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-7 p-1">
+// <div class="card p-1 gif-card">
+// <h4 class="card-title text-center p-2 mx-2  bg-primary text-light">raiting</h4>
+// <img src="" data-still="" data-animate="" data-state="still" class="px-1 m-1" alt=${alt}>
+// </div></div>
 function mkImgCard(animalImgObj, alt) {
   let _col = document.createElement('div');
   _col.setAttribute('class', 'col-xl-2 col-lg-3 col-md-4 col-sm-6 col-7 p-1');
@@ -70,50 +63,57 @@ function mkImgCard(animalImgObj, alt) {
   _col.appendChild(_card);
 
   return _col;
-
-  // loop the obj and create a img for each key TODO:
-  // for (let _key of Object.keys(animalImgObj)) {
-  //   // Create a new card with the current obj
-  //   let _newCard = mkImgCard(_dataObj[_key], search);
-  //   // add current card to parrent
-  //   _parent.append(_newCard);
-  // }
+}
+/* ************************************************************* */
+/* * * * * * * * * * * * reloadButtons() * * * * * * * * * * * * */
+/* ************************************************************* */
+// This function will read each element from animalsSet and create a button
+// for each animal
+function reloadButtons() {
+  // Clean Screen to remove current buttons
+  document.querySelector("#btnContainer").innerHTML = "";
+  // Loop trough Set and add new Buttons
+  for (let animal of animalsSet) {
+    let _btn = mkbtn(animal);
+    document.querySelector("#btnContainer").appendChild(_btn);
+  }
 }
 /* ************************************************************ */
-/* * * * * * * * * * * * * loadImages() * * * * * * * * * * * * */
+/* * * * * * * * * * * * * * loadImgs() * * * * * * * * * * * * */
 /* ************************************************************ */
-function loadImages(search) {
-  // remove whitespaces from search string using regex
-  search = search.trim().split(/\s/).join('+');
-  // API URL
-  let _URL = `http://api.giphy.com/v1/gifs/search?q=${search}&api_key=${API_KEY}d&limit=${IMG_LIMIT}&rating=pg13`;
+function loadImgs(imgObject) {
   // Get the parent element
   let _parent = document.querySelector("#imgsContainer");
+  // loop the obj and create a img for each key
+  for (let _key of Object.keys(imgObject)) {
+    // Create a new card with the current obj
+    let _newCard = mkImgCard(imgObject[_key], queryURL.q);
+    // add current card to parrent
+    _parent.append(_newCard);
+  }
+}
+/* *********************************************************** */
+/* * * * * * * * * * * * getImagesObject() * * * * * * * * * * */
+/* *********************************************************** */
+function getImagesObject() {
+  // API URL
+  let _url = `${queryURL.url}q=${queryURL.q}&api_key=${queryURL.apiKey}d&limit=${queryURL.limit}&rating=pg13`;
   // Check if fetch is available
-  //////////// ****************** TODO: added ! for testing with XMLHttpRequest ******************\\\\\\\\\\\\\
-  if (!window.fetch) {
+  if (window.fetch) {
     // send request
-    fetch(_URL)
+    fetch(_url)
       // Parse the resonse
       .then((_response) => _response.json())
       // Get the Data object from API
       .then((_jsonObj) => _jsonObj.data)
-      .then((_dataObj) => {
-        // loop the obj and create a img for each key
-        for (let _key of Object.keys(_dataObj)) {
-          // Create a new card with the current obj
-          let _newCard = mkImgCard(_dataObj[_key], search);
-          // add current card to parrent
-          _parent.append(_newCard);
-        }
-      });
+      .then(loadImgs);
   }
   // If Fetch not Available
   else {
     // instanciate a XHR
     const xhr = new XMLHttpRequest();
     // Send the reuest
-    xhr.open('GET', _URL);
+    xhr.open('GET', _url);
     // onload event listener
     xhr.onload = () => {
       // If ready state its done
@@ -122,14 +122,9 @@ function loadImages(search) {
         if (xhr.status === 200) {
           let _response = JSON.parse(xhr.responseText);
           let _result = _response.data;
-          console.log(_result);
-          // loop the obj and create a img for each key
-          for (let _key of Object.keys(_result)) {
-            // Create a new card with the current obj
-            let _newCard = mkImgCard(_result[_key], search);
-            // add current card to parrent
-            _parent.append(_newCard);
-          }
+
+          loadImgs(_result);
+
         } else {
           console.log("Error loadin data");
         }
@@ -147,9 +142,11 @@ function onButtonClick(event) {
   // Clear any <img> available at the parent element
   document.querySelector("#imgsContainer").innerHTML = "";
   // get the animal's name of clicked button
-  let _animal = event.target.innerHTML;
+  let _currentAnimal = event.target.innerHTML;
+  // remove whitespaces from search string using regex and add it to queryURL
+  queryURL.q = _currentAnimal.trim().split(/\s/).join('+');
   // add images to view
-  loadImages(_animal);
+  getImagesObject();
 }
 /* =============================================================== */
 /*                     Image Event listener                        */
@@ -196,6 +193,8 @@ function onAddAnimalClick(event) {
     // display error if input not available
     alert("Missing Input");
   }
+  // clear text inout
+  _inputElement.value = "";
 }
 //////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////
